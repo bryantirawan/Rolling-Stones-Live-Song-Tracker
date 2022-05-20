798d3ca988f20df235e2d95830a87ee86dc7b522
bryantirawan <bryantirawan@gmail.com>
2022-05-20 16:26:29 -0700

    trying manytomany

--- concerts/models.py
+++ concerts/models.py
@@ -10,7 +10,7 @@ class Concert(models.Model):
     date = models.DateField(null=True) 
     city = models.CharField(max_length=100, null=True)
     country = models.CharField(max_length=200, null=True)
-    user = models.ForeignKey(USER_MODEL, related_name="concerts", on_delete=models.CASCADE, null=True) 
+    user = models.ManyToManyField(USER_MODEL, related_name="concerts", null=True) 
     song = models.ManyToManyField("Song", blank=True, null=True)
 
     def __str__(self): 
@@ -18,7 +18,7 @@ class Concert(models.Model):
 
 class Song(models.Model): 
     name = models.CharField(max_length = 100, null=True) 
-    user = models.ForeignKey(USER_MODEL, related_name="songs", on_delete=models.CASCADE, null=True)
+    user = models.ManyToManyField(USER_MODEL, related_name="songs", null=True)
 
     def __str__(self):
         return self.name
